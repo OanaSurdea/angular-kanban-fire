2481c8be6ba13273b388df7bfa3c3ad23d5e77e9
OanaSurdea <oana.surdea@ness.com>
2023-02-13 10:53:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kanban-fire
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kanban-fire)
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCYgBLnjAKgAgsvU-Wtuv_JSXilCpFM2lQ',
+    authDomain: 'kanbanfire-bf23e.firebaseapp.com',
+       projectId: 'kanbanfire-bf23e',
+    storageBucket: 'kanbanfire-bf23e.appspot.com',
+    messagingSenderId: '132413927016',
+    appId: '1:132413927016:web:262b06dd296ae01b6215dd',
+    measurementId: 'G-V9QBFXN37G',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
